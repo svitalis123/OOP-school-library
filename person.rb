d997939ab88a4025f4e84583a1d40f828b9f0892
vitalis <vitalismutwiri2gmail.com>
2023-01-03 01:28:16 +0300

    Added decorateor pattern

--- person.rb
+++ person.rb
@@ -1,5 +1,12 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method #{__method__}"
+  end
+end
+
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
@@ -7,6 +14,10 @@ class Person
   end
   attr_accessor :name, :age
   attr_reader :id
+
+  def correct_name
+    @name
+  end
 
   def can_use_services?
     of_age? || @parent_permission == true
@@ -18,3 +29,34 @@ class Person
     @age >= 18
   end
 end
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.split.map(&:capitalize).join(' ')
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.size > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+Capitalized_Person = CapitalizeDecorator.new(person)
+print Capitalized_Person.correct_name
+puts "\n\n"
+Capitalized_Trimmed_Person = TrimmerDecorator.new(Capitalized_Person)
+print Capitalized_Trimmed_Person.correct_name
