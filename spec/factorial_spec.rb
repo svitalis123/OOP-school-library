a54527f3ea8862723ea4912ea82799cb4a98639f
vitalis <vitalismutwiri2gmail.com>
2023-01-12 14:37:42 +0300

    added test

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,8 +1,23 @@
-describe Factorial do
-  context "When passed a number to find factorial" do
+describe Solver do
+  context "When passed an input" do
+    solver=Solver.new
     it "expect factorial of 5 tobe 120" do
-      factorial=Factorial.new
-      expect(factorial.factorial(5)).to eq 120
+      expect(solver.factorial(5)).to eq 120
+    end
+    it "expect positive number " do
+      expect(solver.factorial(5)).to >= 0
+    end
+    it "Expect hello to be reversed to olleh" do
+      expect(solver.reverse("hello")).to eq "olleh"
+    end
+    it "Expect 6 to return fizz" do
+      expect(solver.fizzbuzz(6)).to eq "fizz"
+    end
+    it "Expect 10 to return buzz" do
+      expect(solver.fizzbuzz(10)).to eq "buzz"
+    end
+    it "Expect 15 to return fizzbuzz" do
+      expect(solver.fizzbuzz(15)).to eq "fizzbuzz"
     end
   end
 
